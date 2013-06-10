ec6ef454627d2ed6488bb39d82cd9028604fe623
Mar <mar@MacBook-Pro-de-Mar.local>
2013-06-10 21:45:38 +0200

    Referencias y cambio de nombres

---
+++ 1/holaMundoCompleto.py
@@ -0,0 +1,11 @@
+""" Esto es un Hola mundo! """
+
+def hello():
+	"""Esto es una funcion que imprime "Hola mundo!" """
+	print "Hola mundo!"
+
+def main():
+	hello()
+
+if __name__ == "__main__":
+	main()
