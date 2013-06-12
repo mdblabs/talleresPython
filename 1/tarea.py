3e0b92bd82457fbb7cae5d9e5ea66e5bd3b7abb9
Mar <mar@MacBook-Pro-de-Mar.local>
2013-06-12 17:47:10 +0200

    Solucion subida 1

---
+++ 1/tarea.py
@@ -0,0 +1,24 @@
+def suma():
+	a=2
+	b=3
+	print a+b
+def resta():
+	a=2
+	b=3
+	print b-a
+def multiplicacion():
+	a=2
+	b=3
+	print a*b
+def division():
+	a=2
+	b=3
+	print a/b
+
+if __name__=='__main__':
+	print "Programa calculadora"
+	suma()
+	resta()
+	multiplicacion()
+	division()
+	
